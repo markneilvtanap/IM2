cc123f2fb293b0ff0bb343442d741505ce5ae42a
mark neil <markneilvtanap@gmail.com>
2024-01-05 21:09:31 +0800

    push updated IM2 project

---
+++ endterm_IM2/endterm_IM2/Doctor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace endterm_IM2
+{
+    public partial class Doctor : Form
+    {
+        public Doctor()
+        {
+            InitializeComponent();
+        }
+
+        private void panel5_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            login login = new login();
+            login.ShowDialog();
+        }
+
+        private void panel2_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            this.Dispose();
+            login login = new login();
+            login.ShowDialog();
+        }
+    }
+}
